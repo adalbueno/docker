0b21af7020715a4875903ccadd38e5960709eb83
Adalberto Diniz Bueno Junior <adalbueno@MacBook-Pro.local>
2020-05-05 12:01:20 +0200

    Adjust aliases.sh

--- aliases.sh
+++ aliases.sh
@@ -6,12 +6,9 @@ cd -
 
 DOCKER_COMPOSE_FILE="${DIR}/docker-compose.yml"
 
-alias npm_dashboard="docker-compose -f ${DOCKER_COMPOSE_FILE} exec dashboard npm"
+alias npm_app="docker-compose -f ${DOCKER_COMPOSE_FILE} exec app npm"
 alias npm_api="docker-compose -f ${DOCKER_COMPOSE_FILE} exec api npm"
 
 # PHP related aliases
-alias composer_api="docker-compose -f ${DOCKER_COMPOSE_FILE} exec api composer"
+alias php_app="docker-compose -f ${DOCKER_COMPOSE_FILE} exec app php"
 alias php_api="docker-compose -f ${DOCKER_COMPOSE_FILE} exec api php"
-
-alias composer_dashboard="docker-compose -f ${DOCKER_COMPOSE_FILE} exec dashboard composer"
-alias php_dashboard="docker-compose -f ${DOCKER_COMPOSE_FILE} exec dashboard php"
